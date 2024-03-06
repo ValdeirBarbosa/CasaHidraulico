3a13fb69c13a278dce405e00dff280ac2cf12169
Barbosa, Valdeir <barbov1@medtronic.com>
2024-03-06 16:20:24 -0300

    feat - creating controlles ans AppError

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -1,9 +1,14 @@
+const  AppError = require("../utils/AppError")
 class UsersControllers {
   /**  Index  - GET lista todos registros | Show - GET para exibir um usuário especifico | Create -POST criar um registro  | update - PUT atualizar um registros | delete - DELETE para remover um registro */
 
 
   create(request, response) {
     const { name, email, password } = request.body;
+
+    if(!name){
+      throw new AppError("O nome e obrigatório!");
+    }
     return response.status(201).json({ name, email, password });
 
   }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,7 +3,17 @@ const  { Router } = require( "express");
 const UserControllers = require("../controllers/UsersControllers");
 
 const userRoutes = Router();
+
+//exemplo de middleware
+function MyMiddleware(request, response, next){
+  console.log("Passou pelo MyMiddleware!!!");
+  console.log(request.body)
+  next()
+
+}
+
+
 const userControllers = new UserControllers();
 
-userRoutes.get("/", userControllers.create);
+userRoutes.get("/",userControllers.create);
 module.exports = userRoutes;
