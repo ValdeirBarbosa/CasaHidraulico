82f612d42f10dc39d4c438225fae7be92913401f
ValdeirBarbosa <valdeir.barbosa@outlook.com>
2024-03-06 22:24:55 -0300

    feat - starting migrations

---
+++ src/database/sqlite/migrations/createUsers.js
@@ -0,0 +1,11 @@
+const createUsers=`
+CREATE TABLE IF NOT EXISTS user(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR,
+  email VARCHAR,
+  password VARCHAR,
+  avatar VARCHAR NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)`
+
+module.exports = createUsers
